2935bdc809c880138556548a70b240e119266031
shan tirmizi <tirmizishahnawaz@gmail.com>
2020-11-10 15:24:08 +0000

    finsied2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do 
+	Task.create(
+		title: Faker::Company.name,
+		details: Faker::Lorem.sentence(word_count: 7),
+		completed: Faker::Boolean.boolean
+	)
+end
